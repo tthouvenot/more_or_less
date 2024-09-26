34f5599611cd1414c0d8740c0c83b3004ca4105f
tthouvenot <tristan.thouvenot57@gmail.com>
2024-09-26 18:21:26 +0200

    docu: game loop done. Created functions game_loop and exit_program.

--- more_or_less.py
+++ more_or_less.py
@@ -23,20 +23,139 @@
 #       - on lance le jeu
 #   - quitter
 #
-# Game Rules:
-#   - l'utilisateur à 15 essaies
-#   - chaque essaie lui fait perdre 3 points
-#   - une victoire lui donne 100 points
+# //Game Rules:
+#   //- l'utilisateur à 15 essaies
+#   //- chaque essaie lui fait perdre 3 points
+#   //- une victoire lui donne 100 points
 #
 # Game Loop:
-# - on sélectionne un int random entre 0 et 10000
-# - On demande à l'utilisateur
-# - si sa réponse est:
-#   - plus basse on lui dit c'est plus et on lui redemande
-#   - plus haute on lui dit c'est moins et on lui redemande
-#   - pas un chiffre entre 0 et 10000:
-#       - on le prévient et il perd ni un essaie ni des points
-# - La partie finie s'il a trouvé le bon chiffre ou plus aucun essaie
-# - On lui demande s'il veut rejouer
-#   - oui on relance le jeu
-#   - non on enregistre les datas dans le fichier JSON et on fait un résumé de ses stats. On lui demande d'appuyer sur un bouton pour quitter le programme
+# // - on sélectionne un int random entre 0 et 10000
+# //- On demande à l'utilisateur
+# //- si sa réponse est:
+#   //- plus basse on lui dit c'est plus et on lui redemande
+#   //- plus haute on lui dit c'est moins et on lui redemande
+#   //- pas un chiffre entre 0 et 10000:
+#      // - on le prévient et il perd ni un essaie ni des points
+# //- La partie finie s'il a trouvé le bon chiffre ou plus aucun essaie
+#// - On lui demande s'il veut rejouer
+#//   - oui on relance le jeu
+#   - non :
+#       - on enregistre les datas dans le fichier JSON 
+#       - on fait un résumé de ses stats. 
+#       //- On lui demande d'appuyer sur un bouton pour quitter le programme
+
+
+import random
+import time
+
+#################
+### variables ###
+#################
+
+still_playing = True
+total_points = 0
+
+#################
+### functions ###
+#################
+
+def game_loop(rand):
+    
+    tries = 0
+    end_of_game = False
+    current_point = 0
+
+# TODO: Change tries when program is finished
+    while tries < 1 and not end_of_game:
+        try:
+            user_answer = int(input("Quel est votre chiffre? "))
+
+            if user_answer < rand and user_answer <= 10000:
+                print("C'est plus!")
+                tries += 1
+                current_point -= 3
+            elif user_answer > rand and user_answer >= 0:
+                print("C'est moins!")
+                tries += 1
+                current_point -= 3
+            elif user_answer == rand:
+                print(f"Bravo! La bonne réponse était bien {rand}!")
+                current_point += 100
+                end_of_game = True
+            else:
+                print("Attention, le chiffre à deviner est entre 0 et 10.000")
+
+        except:
+            print("Veuillez entrer un chiffre entre 0 et 10000")
+    
+    return current_point
+
+def exit_program():
+
+    quit_game = False
+
+    while not quit_game:
+        button_press = input("Merci de votre participation. Veuillez appuyer sur la touche \"q\" pour quitter le jeu!: ").lower()
+
+        if button_press != "q":
+            print("Erreur: saisie incorrect!")
+        else:
+            quit_game = True
+
+#################
+### Game Loop ###
+#################
+
+while still_playing:
+    print("###############################")
+    print("###                         ###")
+    print("### Bienvenue au juste prix ###")
+    print("###                         ###")
+    print("###############################")
+    print()
+
+    print("Les règles sont simples. Vous devez deviner un chiffre entre 0 et 10.000")
+    print("Si votre réponse est inférieure je dirai c'est plus")
+    print("Si votre réponse est supérieure je dirai c'est moins")
+    print("Attention:")
+    print("Vous avez 15 essaies, chaque mauvaise réponse vous perdez 3 points")
+    print("Si vous trouvez la bonne réponse vous gagnerez 100 points.")
+    print()
+
+    print("3...", end='', flush=True)
+    time.sleep(1)
+    print("2...", end='', flush=True)
+    time.sleep(1)
+    print("1...", end='', flush=True)
+    time.sleep(1)
+    print("C'est parti!")
+    print()
+
+    win = 0
+    lose = 0
+    random_number = random.randint(0, 10000)
+
+    result = game_loop(random_number)
+
+    if result == 0:
+        print(f"Dommage, le bon chiffre à trouver était : {random_number}.")
+        lose +=1
+        win = 0
+    elif result >= 0:
+        total_points += result
+        win += 1
+        lose = 0
+
+    replay = input("Voulez-vous retenter votre chance? (oui ou non): ").lower()
+    print()
+
+    if replay == "non" or replay == "n":
+        still_playing = False
+
+######################
+### End of program ###
+######################
+
+exit_program()
+print("A bientôt!")
+time.sleep(2)
