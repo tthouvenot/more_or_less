9cd42123dd01d60b7813d6a98ff0fb805dd88993
tthouvenot <tristan.thouvenot57@gmail.com>
2024-09-26 19:32:39 +0200

    docu: JSON access done. Created functions json_access and game_instance

--- more_or_less.py
+++ more_or_less.py
@@ -3,11 +3,12 @@
 ###     Jeu Plus ou Moins     ###
 ###                           ###
 ### Author: Tristan Thouvenot ###
-### Version: 0.1              ###
+### Version: 0.2              ###
 ### Date: 26/09/2024          ###
 #################################
 
 # L'objectif est de créer le jeu du juste prix. 
+# //On télécharge les données du fichier JSON
 # L'utilisateur va fournir son nom
 #   Si il est présent dans le fichier data:
 #       - on récupère ses données
@@ -40,13 +41,13 @@
 #// - On lui demande s'il veut rejouer
 #//   - oui on relance le jeu
 #   - non :
-#       - on enregistre les datas dans le fichier JSON 
+#       //- on enregistre les datas dans le fichier JSON 
 #       - on fait un résumé de ses stats. 
 #       //- On lui demande d'appuyer sur un bouton pour quitter le programme
 
-
 import random
 import time
+import json
 
 #################
 ### variables ###
@@ -54,10 +55,35 @@ import time
 
 still_playing = True
 total_points = 0
+user_data = None
+updated_datas = None
 
 #################
 ### functions ###
 #################
+
+def json_access(task, datas = None, updated_datas = None):
+
+    file_path = './user_data.json'
+
+    if task == "read":
+        with open(file_path, 'r') as file:
+            datas_from_files = json.load(file) 
+        return datas_from_files
+    
+    if task == "write":
+        with open(file_path, 'w') as file:
+            json.dump(datas, file, indent=4)
+
+    if task == "update":
+        current_data = json_access("read")
+
+        if 'users' in current_data:
+            current_data['users'].append(updated_datas)
+        else:
+            current_data['users'] = [updated_datas]
+        return current_data
+
 
 def game_loop(rand):
     
@@ -102,55 +128,65 @@ def exit_program():
         else:
             quit_game = True
 
+def game_instance(user):
+    while still_playing:
+        print("###############################")
+        print("###                         ###")
+        print("### Bienvenue au juste prix ###")
+        print("###                         ###")
+        print("###############################")
+        print()
+
+        print("Les règles sont simples. Vous devez deviner un chiffre entre 0 et 10.000")
+        print("Si votre réponse est inférieure je dirai c'est plus")
+        print("Si votre réponse est supérieure je dirai c'est moins")
+        print("Attention:")
+        print("Vous avez 15 essaies, chaque mauvaise réponse vous perdez 3 points")
+        print("Si vous trouvez la bonne réponse vous gagnerez 100 points.")
+        print()
+
+        print("3...", end='', flush=True)
+        time.sleep(1)
+        print("2...", end='', flush=True)
+        time.sleep(1)
+        print("1...", end='', flush=True)
+        time.sleep(1)
+        print("C'est parti!")
+        print()
+
+        win = 0
+        lose = 0
+        random_number = random.randint(0, 10000)
+
+        result = game_loop(random_number)
+
+        if result == 0:
+            print(f"Dommage, le bon chiffre à trouver était : {random_number}.")
+            lose +=1
+            win = 0
+        elif result >= 0:
+            total_points += result
+            win += 1
+            lose = 0
+
+        replay = input("Voulez-vous retenter votre chance? (oui ou non): ").lower()
+        print()
+
+        if replay == "non" or replay == "n":
+            json_access('write', file_data)
+            still_playing = False
+
+######################
+### Welcoming User ###
+######################
+
+file_data = json_access('read')
+
 #################
 ### Game Loop ###
 #################
 
-while still_playing:
-    print("###############################")
-    print("###                         ###")
-    print("### Bienvenue au juste prix ###")
-    print("###                         ###")
-    print("###############################")
-    print()
-
-    print("Les règles sont simples. Vous devez deviner un chiffre entre 0 et 10.000")
-    print("Si votre réponse est inférieure je dirai c'est plus")
-    print("Si votre réponse est supérieure je dirai c'est moins")
-    print("Attention:")
-    print("Vous avez 15 essaies, chaque mauvaise réponse vous perdez 3 points")
-    print("Si vous trouvez la bonne réponse vous gagnerez 100 points.")
-    print()
-
-    print("3...", end='', flush=True)
-    time.sleep(1)
-    print("2...", end='', flush=True)
-    time.sleep(1)
-    print("1...", end='', flush=True)
-    time.sleep(1)
-    print("C'est parti!")
-    print()
-
-    win = 0
-    lose = 0
-    random_number = random.randint(0, 10000)
-
-    result = game_loop(random_number)
-
-    if result == 0:
-        print(f"Dommage, le bon chiffre à trouver était : {random_number}.")
-        lose +=1
-        win = 0
-    elif result >= 0:
-        total_points += result
-        win += 1
-        lose = 0
-
-    replay = input("Voulez-vous retenter votre chance? (oui ou non): ").lower()
-    print()
-
-    if replay == "non" or replay == "n":
-        still_playing = False
+game_instance(user_data)
 
 ######################
 ### End of program ###
